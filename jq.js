da88b53e1b3035a334ed322ca091118e5d227603
lusijanka <lucjamiodek@gmail.com>
2017-08-17 14:14:34 +0200

    function animate fixed

--- jq.js
+++ jq.js
@@ -1,10 +1,33 @@
 $(function() {
-	var carouselList = $("#carousel ul");
+	var carouselList = $('#carousel ul');
 	setInterval(changeSlide, 3000);
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide) {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide)
+		function moveFirstSlide() {
+			var firstItem = carouselList.find('li:first');
+			var lastItem = carouselList.find('li:last');
+			lastItem.after(firstItem);
+			carouselList.css({marginLeft:0});
+		};
+	};
+
+	$('#right').click(function() {
+		clearInterval(startAction);
+		changeLastSlide();
+	});
+	
+	$('#left').click(function() {
+		clearInterval(startAction);
+		changeSlide();
+	});
+	
+	function changeLastSlide() {
+		carouselList.animate({'marginRight':-400}, 500, moveLastSlide)
+		function moveLastSlide() {
+			var firstItem = carouselList.find('li:first');
+			var lastItem = carouselList.find('li:last');
+			firstItem.after(lastItem);
+			carouselList.css({marginRight:0});
+		};
 	};
 });
